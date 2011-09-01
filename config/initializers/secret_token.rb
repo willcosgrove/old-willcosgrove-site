5b712a74dde21eb44cf1c8c44e96cd545792e0f1
Will Cosgrove <will@willcosgrove.com>
2011-09-01 18:28:56 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Willcosgrove::Application.config.secret_token = '9b9ef5fde9c3285d70d894066724020f13079c9c026895632ed0d9b22641745d9b349f807841279efec83fab6e9e7b874dc329cd25fc89443c323f6f17085d4b'
